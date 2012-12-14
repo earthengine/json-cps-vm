835be700c4248b36d1d016a6b2b3005c617702ff
earthengine <earthengine@gmail.com>
2012-12-14 17:05:03 +1100

    New implementation of getBind (now named as getCallSpec

--- JavaScript/jsonvm.js
+++ JavaScript/jsonvm.js
@@ -97,81 +97,71 @@ function RunEngine() {
 
 function JsonVM(code, engine) {
     var entries = [];
-    var waitting = { name: "waiting" };
+    var waiting = { name: "waiting" };
 
-    var getRefItem = function (refitem, ent, binds, args) {
-        if (refitem.type === "entry") return ent[refitem.index];
+    var getRefItem = function (refitem, binds, args) {
+        if (refitem.type === "entry") return entries[refitem.index];
         else if (refitem.type === "bind") return binds[refitem.index];
         else if (refitem.type === "param") return args[refitem.index];
         else if (refitem.type === "extern") return engine.getExtern(refitem.name);
-        else if (refitem.type === "waitting") return waitting;
     };
-    var getBind = function (callspec, ent, binds, args) {
-        var objs = [];
-        var mapping = [];
-        var callee = getRefItem(callspec.callee, ent, binds, args);
+    var getCallSpec = function (callspec, binds, args) {
+        var waitings = [];
+        var bond_values = [];
+        var callee = getRefItem(callspec.callee, binds, args);
         for (var i = 0; i < callspec.params.length; ++i) {
-            objs.push(getRefItem(callspec.params[i], ent, binds, args));
-        }
-        if (typeof (callee) === "undefined") {
-            console.log(callspec);
-        }
-        if (typeof (callee.setparams) !== "undefined") {
-            var currentidx = 0;
-            for (var i = 0; i < callee.params.length; ++i) {
-                if (typeof (callee.params[i]) === "undefined") {
-                    mapping[currentidx++] = i;
-                }
+            var refitem = getRefItem(callspec.params, binds, args);
+            if (callspec.params[i].type === "waiting") {
+                waitings.push(i);
+                bond_values.push(waiting);
             }
-            mapping[currentidx] = i;
+            else {
+                bond_values.push(refitem);
+            }
         }
-        function f(args) {
-            for (var i = 0; i < objs.length; ++i) {
-                if (args.length === 0) break;
-                if (typeof (objs[i]) === "undefined") {
-                    if (args[0] !== waitting) objs[i] = args[0];
-                    args.shift();
+        var f_call = function () {
+            var argms = [].slice.apply(arguments);
+            var params = bond_values.slice();
+            for (var i = 0; i < params.length; ++i) {
+                if (typeof (params[i]) !== "undefined"
+                    && params[i] !== waiting) continue;
+                if (argms.length !== 0 && (waitings.indexOf(i) < 0
+                    || bond_values[i] === waiting)) {
+                    params[i] = argms.shift();
+                    if (bond_values[i] === waiting) bond_values[i] = params[i];
                 }
+                else if (params[i] === waiting)
+                    delete params[i];
             }
-            if (typeof (callee.params) !== "undefined") {
-                objs = objs.concat(args);
-                for (var i = 0; i < objs.length; ++i) {
-                    if (objs[i] !== waitting) {
-                        var j = mapping[i];
-                        if (j > 0)
-                            callee.setparams(j, objs[i]);
-                        //callee.params[j]=objs[i];
-                    }
-                }
-                engine.callobj.apply(this, [callee]);
-            } else {
-                objs = objs.concat(args);
-                engine.callobj.apply(this, [callee].concat(objs));
-            }
+            engine.callobj.apply(this, [callee].concat(params));
         };
-        function f_bind() { f([].slice.apply(arguments)); };
-        f_bind.setparams = function (i, v) {
-            objs[i] = v;
-        };
-        f_bind.params = objs;
-        return f_bind;
+        f_call.waitings = waitings;
+        return f_call;
     };
 
     var getEntry = function (entry) {
-        function f_entry(args) {
+        var f_entry = function () {
+            var args = [].slice.apply(arguments);
             var binds = [];
             entry.binds.forEach(function (bind) {
-                binds.push(getBind(bind, entries, binds, args));
+                var f_call = getCallSpec(bind, binds, args);
+                f_call.f_name = entry.name + "#" + binds.length + "." + entry.count;
+                binds.push(f_call);
             });
             entry.execs.forEach(function (exec) {
-                getBind(exec, entries, binds, args)();
+                getCallSpec(exec, binds, args)();
             });
+            entry.count++;
         };
-        return function () { f_entry([].slice.apply(arguments)); };
+        return f_entry;
     };
 
     code.entries.forEach(function (entry) {
-        entries.push(getEntry(entry));
+        var f_entry = getEntry(entry);
+        entry.name =
+        f_entry.f_name = "!" + entries.length;
+        entry.count = 0;
+        entries.push(f_entry);
     });
 
     this.runExport = function (name) {
@@ -202,7 +192,7 @@ window.onload = function () {
         if (vm.hasExport("Concurrent"))
             vm.runExport("Concurrent", 20, 10, function (n) { alert(n); });
         else if (vm.hasExport("Factorial"))
-            vm.runExport("Factorial", 3, function (n) { alert(n); });
+            vm.runExport("Factorial", 2, function (n) { alert(n); });
         else if (vm.hasExport("Concat"))
             vm.runExport("Concat", [1, 3, 4], [5, 6, 9], function (l) { array_iter(engine, l); });
         else if (vm.hasExport("Filter"))
@@ -210,7 +200,7 @@ window.onload = function () {
                 if (i > 4) whentrue(); else whenfalse();
             }, function (l) { array_iter(engine, l); });
         else if (vm.hasExport("yinyang")) {
-            engine.setLimit(10000);
+            engine.setLimit(100);
             vm.runExport("yinyang");
         }
     };
